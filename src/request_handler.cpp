f5b0066d978d55272d63bf91795f5bc63de4db40
ubermensch04 <sparsh.sg04@gmail.com>
2025-04-16 14:21:48 +0530

    Reading request body and creating a file

--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -62,6 +62,136 @@ std::unordered_map<std::string, std::string> parse_headers(const std::string& he
     return header_data;
 }
 
+std::string handle_GET_request(const std::string& request_target, const std::string& directory, const std::string& headers) 
+{
+  //Storing Header data in a HashMap
+  std::unordered_map<std::string,std::string> header_data;
+  header_data=parse_headers(headers);
+  
+  std::string response_str;
+
+  //Generating Response
+  const std::string echo_prefix = "/echo/";
+  const std::string file_prefix = "/files/";
+  if (request_target == "/") 
+  {
+    response_str = "HTTP/1.1 200 OK\r\n\r\n";
+  } 
+  else if (request_target.rfind(echo_prefix, 0) == 0)
+  {
+    
+    std::string req_string = request_target.substr(echo_prefix.length());
+    std::ostringstream oss;
+    oss << "HTTP/1.1 200 OK\r\n"
+        << "Content-Type: text/plain\r\n"
+        << "Content-Length: " << req_string.length() << "\r\n"
+        << "\r\n"
+        << req_string;
+    response_str = oss.str();
+  }
+  else if(request_target.rfind(file_prefix, 0) == 0)
+  {
+    std::string file_name = request_target.substr(file_prefix.length());
+    if (file_name.empty() || file_name.find("..") != std::string::npos) 
+    {
+      std::cerr << "Attempted path traversal or empty filename: " << file_name << "\n";
+      response_str = "HTTP/1.1 400 Bad Request\r\n\r\n";
+      // Early return might be cleaner here if not setting response_str
+      // return "HTTP/1.1 400 Bad Request\r\n\r\n";
+    }
+    else
+    {
+      std::string full_file_path = directory + "/" + file_name;
+      std::ifstream file(full_file_path,std::ios::binary);
+      if(!file)
+      {
+        std::cerr << "File not found: " << full_file_path << "\n";
+        response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
+      else
+      {
+        //Reading the file contents
+        file.seekg(0, std::ios::end);
+        size_t file_size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        std::string file_content(file_size, '\0');
+        file.read(&file_content[0], file_size);
+
+        //Generating the response
+        std::ostringstream oss;
+        oss << "HTTP/1.1 200 OK\r\n"
+            << "Content-Type: application/octet-stream\r\n"
+            << "Content-Length: " << file_size << "\r\n"
+            << "\r\n" 
+            << file_content;
+        response_str = oss.str();
+        std::cout << "File sent: " << full_file_path << "\n";
+      }
+    }
+  }
+  else if(request_target=="/headers")
+  {
+    std::ostringstream oss;
+    oss << "HTTP/1.1 200 OK\r\n"
+        << "Content-Type: text/plain\r\n"
+        << "Content-Length: " << headers.length() << "\r\n"
+        << "\r\n" 
+        << headers;
+    response_str = oss.str();
+  }
+  else if(request_target=="/user-agent")
+  {
+    std::string user_agent = header_data["user-agent"];
+    std::string status_line = "HTTP/1.1 200 OK\r\n";
+    std::string content_type = "Content-Type: text/plain\r\n";
+    std::string content_length = "Content-Length: " + std::to_string(user_agent.length()) + "\r\n";
+    std::ostringstream oss;
+    oss << status_line << content_type << content_length << "\r\n" << user_agent;
+    response_str = oss.str();
+  }
+  else 
+  {
+    response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
+  return response_str;
+    
+}
+
+std::string handle_PUT_request(const std::string& request_target, const std::string& directory, const std::string& headers, const std::string& body) 
+{
+  //Storing Header data in a HashMap
+  std::unordered_map<std::string,std::string> header_data;
+  header_data=parse_headers(headers);
+  
+  std::string response_str;
+  const std::string file_prefix = "/files/";
+  if(request_target.rfind(file_prefix, 0) == 0)
+  {
+    std::string file_name = request_target.substr(file_prefix.length());
+    if(file_name.empty())
+    {
+      std::cerr << "Empty filename in PUT request\n";
+      response_str = "HTTP/1.1 400 Bad Request\r\n\r\n";
+    }
+    else
+    {
+      std::string full_file_path=directory+"/"+file_name;
+      std::ofstream file(full_file_path,std::ios::binary);
+      file.write(body.c_str(), body.length());
+      if(file)
+      {
+        std::cout << "File created: " << full_file_path << "\n";
+        response_str = "HTTP/1.1 201 Created\r\n\r\n";
+      }
+      else
+      {
+        std::cerr << "Failed to create file: " << full_file_path << "\n";
+        response_str = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+      }
+    }
+  }
+  return response_str;
+}
 std::string handle_connection(int client_fd,const std::string& directory) 
 {
     std::string request_data;
@@ -81,11 +211,6 @@ std::string handle_connection(int client_fd,const std::string& directory)
         else
         {
           request_data.append(buffer,bytes_received);
-          if(request_data.find(EOH)!=std::string::npos)
-          {
-            request_data.resize(request_data.find(EOH) + EOH.length());
-            break;
-          }
         }
     }
 
@@ -99,100 +224,39 @@ std::string handle_connection(int client_fd,const std::string& directory)
     std::string request_line=request_data.substr(0,first_crlf);
     size_t EOH_pos = request_data.find(EOH);
     std::string headers=request_data.substr(first_crlf+2,EOH_pos-first_crlf-2);
-    
+    std::string body=request_data.substr(EOH_pos+4);
+
+    //Finding the request method
+    size_t method_end = request_line.find(' ');
+    if (method_end == std::string::npos) 
+    {
+        std::cerr << "Invalid request line\n";
+        return "";
+    }
+    std::string method = request_line.substr(0, method_end);
+
     //Finding Request Target
     std::string request_target=parse_request_target(request_line);
     if (request_target.empty()) 
     { 
       return ""; 
     }
-    //Storing Header data in a HashMap
-    std::unordered_map<std::string,std::string> header_data;
-    header_data=parse_headers(headers);
 
-    //Generating Response
     std::string response_str;
-    const std::string echo_prefix = "/echo/";
-    const std::string file_prefix = "/files/";
-    if (request_target == "/") 
+    if(strcmp(method.c_str(),"GET")==0)
     {
-      response_str = "HTTP/1.1 200 OK\r\n\r\n";
-    } 
-    else if (request_target.rfind(echo_prefix, 0) == 0)
-    {
-      
-      std::string req_string = request_target.substr(echo_prefix.length());
-      std::ostringstream oss;
-      oss << "HTTP/1.1 200 OK\r\n"
-          << "Content-Type: text/plain\r\n"
-          << "Content-Length: " << req_string.length() << "\r\n"
-          << "\r\n"
-          << req_string;
-      response_str = oss.str();
+      response_str=handle_GET_request(request_target,directory,headers);
     }
-    else if(request_target.rfind(file_prefix, 0) == 0)
+    else if(strcmp(method.c_str(),"POST")==0)
     {
-      std::string file_name = request_target.substr(file_prefix.length());
-      if (file_name.empty() || file_name.find("..") != std::string::npos) 
-      {
-        std::cerr << "Attempted path traversal or empty filename: " << file_name << "\n";
-        response_str = "HTTP/1.1 400 Bad Request\r\n\r\n";
-        // Early return might be cleaner here if not setting response_str
-        // return "HTTP/1.1 400 Bad Request\r\n\r\n";
-      }
-      else
-      {
-        std::string full_file_path = directory + "/" + file_name;
-        std::ifstream file(full_file_path,std::ios::binary);
-        if(!file)
-        {
-          std::cerr << "File not found: " << full_file_path << "\n";
-          response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
-        }
-        else
-        {
-          //Reading the file contents
-          file.seekg(0, std::ios::end);
-          size_t file_size = file.tellg();
-          file.seekg(0, std::ios::beg);
-          std::string file_content(file_size, '\0');
-          file.read(&file_content[0], file_size);
-  
-          //Generating the response
-          std::ostringstream oss;
-          oss << "HTTP/1.1 200 OK\r\n"
-              << "Content-Type: application/octet-stream\r\n"
-              << "Content-Length: " << file_size << "\r\n"
-              << "\r\n" 
-              << file_content;
-          response_str = oss.str();
-          std::cout << "File sent: " << full_file_path << "\n";
-        }
-      }
+      std::cout << "POST request received\n";
+      response_str=handle_PUT_request(request_target,directory,headers,body);
     }
-    else if(request_target=="/headers")
+    else
     {
-      std::ostringstream oss;
-      oss << "HTTP/1.1 200 OK\r\n"
-          << "Content-Type: text/plain\r\n"
-          << "Content-Length: " << headers.length() << "\r\n"
-          << "\r\n" 
-          << headers;
-      response_str = oss.str();
+      std::cerr << "Unsupported HTTP method: " << method << "\n";
+      return "";
     }
-    else if(request_target=="/user-agent")
-    {
-      std::string user_agent = header_data["user-agent"];
-      std::string status_line = "HTTP/1.1 200 OK\r\n";
-      std::string content_type = "Content-Type: text/plain\r\n";
-      std::string content_length = "Content-Length: " + std::to_string(user_agent.length()) + "\r\n";
-      std::ostringstream oss;
-      oss << status_line << content_type << content_length << "\r\n" << user_agent;
-      response_str = oss.str();
-    }
-    else 
-    {
-      response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
-    }
+    
     return response_str;
 }
