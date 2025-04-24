b1c67e30e22e202da453eaf95f9975add253da29
ubermensch04 <sparsh.sg04@gmail.com>
2025-04-24 14:56:35 +0530

    making connections persistent

--- src/request_handler.h
+++ src/request_handler.h
@@ -3,6 +3,6 @@
 #include <string>
 #include <unordered_map>
 
-std::pair<std::string, bool> handle_connection(std::string& request_line,std::string& headers,std::string& body,const std::string& directory);
+std::pair<std::string, bool> handle_connection(const std::string& request_line,const std::string& headers,const std::string& body,const std::string& directory);
 std::unordered_map<std::string, std::string> parse_headers(const std::string& headers_block);
 #endif
